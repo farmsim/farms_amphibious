459bf65b17850658efc40f67d4ba51e4055437ea
Jonathan Arreguit <jarreguit@gmail.com>
2022-06-07 22:22:21 +0200

    [CONTROL] Updated kinematics controller and added features

--- farms_amphibious/control/kinematics.py
+++ farms_amphibious/control/kinematics.py
@@ -10,16 +10,27 @@ def kinematics_interpolation(
         sampling,
         timestep,
         n_iterations,
+        time_vector=None,
 ):
     """Kinematics interpolations"""
-    data_duration = sampling*kinematics.shape[0]
+    data_duration = (
+        time_vector[-1]
+        if time_vector is not None
+        else sampling*kinematics.shape[0]
+    )
     simulation_duration = timestep*n_iterations
-    interp_x = np.arange(0, data_duration, sampling)
+    interp_x = (
+        time_vector
+        if time_vector is not None
+        else np.arange(0, data_duration, sampling)
+    )
     interp_xn = np.arange(0, simulation_duration, timestep)
     assert data_duration >= simulation_duration, (
         f'Data {data_duration} < {simulation_duration} Sim'
     )
-    assert len(interp_x) == kinematics.shape[0]
+    assert len(interp_x) == kinematics.shape[0], (
+        f'{len(interp_x)} != {kinematics.shape[0]} (shape={kinematics.shape})'
+    )
     assert interp_x[-1] >= interp_xn[-1], (
         f'Data[-1] {interp_x[-1]} < {interp_xn[-1]} Sim[-1]'
     )
@@ -42,24 +53,78 @@ class KinematicsController(AnimatController):
             n_iterations,
             animat_data,
             max_torques,
+            invert_motors=False,
+            indices=None,
+            time_index=None,
+            degrees=False,
+            init_time=0,
+            end_time=0,
     ):
         super().__init__(
             joints_names=joints_names,
-            control_types={
-                joint: ControlType.POSITION
-                for joint in joints_names
-            },
             max_torques=max_torques,
         )
-        assert kinematics.shape[1] == len(joints_names), (
-            f'Expected {len(joints_names)} joints,'
-            f' but got {kinematics.shape[1]}'
+        # Time index
+        if time_index is not None:
+            time_vector = kinematics[:, time_index]
+            time_vector -= time_vector[0]
+        else:
+            time_vector = None
+        # Indices
+        if indices:
+            kinematics = kinematics[:, indices]
+        assert kinematics.shape[1] == len(joints_names[ControlType.POSITION]), (
+            f'Expected {len(joints_names[ControlType.POSITION])} joints,'
+            f' but got {kinematics.shape[1]} (shape={kinematics.shape}'
+            f', indices={indices})'
         )
+        # Converting to radians
+        if degrees:
+            kinematics = np.deg2rad(kinematics)
+        # Invert motors
+        if invert_motors:
+            kinematics *= -1
+        # Add initial time
+        if init_time > 0:
+            kinematics = np.insert(
+                arr=kinematics,
+                obj=0,
+                values=np.repeat(
+                    a=[kinematics[0, :]],
+                    repeats=int(init_time/sampling)+1,
+                    axis=0,
+                ),
+                axis=0,
+            )
+        # Add end time
+        if end_time > 0:
+            kinematics = np.insert(
+                arr=kinematics,
+                obj=kinematics.shape[0],
+                values=np.repeat(
+                    a=[kinematics[-1, :]],
+                    repeats=int(end_time/sampling)+1,
+                    axis=0,
+                ),
+                axis=0,
+            )
+            if time_vector is not None:
+                time_vector = np.insert(
+                    arr=time_vector,
+                    obj=time_vector.shape[0],
+                    values=np.linspace(
+                        time_vector[-1]+timestep,
+                        time_vector[-1]+end_time,
+                        int(end_time/sampling)+1,
+                    ),
+                )
+
         self.kinematics = kinematics_interpolation(
             kinematics=kinematics,
             sampling=sampling,
             timestep=timestep,
             n_iterations=n_iterations,
+            time_vector=time_vector,
         )
         self.animat_data = animat_data
 
