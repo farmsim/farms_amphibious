c246d5dfcc1afe1297009a637af99587edf8eb1e
Jonathan Arreguit <jarreguit@gmail.com>
2022-09-05 21:51:12 +0200

    [SCRIPTS] Added script for plotting connectivity

---
+++ scripts/plot_conectivity.py
@@ -0,0 +1,93 @@
+"""Plot data"""
+
+import os
+import argparse
+
+from cycler import cycler
+import matplotlib.pyplot as plt
+
+from farms_core import pylog
+from farms_amphibious.data.data import AmphibiousData
+from farms_amphibious.model.options import AmphibiousOptions
+from farms_amphibious.utils.network import plot_networks_maps
+
+plt.rc('axes', prop_cycle=(
+    cycler(linestyle=['-', '--', '-.', ':'])
+    * cycler(color=plt.rcParams['axes.prop_cycle'].by_key()['color'])
+))
+plt.rcParams.update({
+    'text.usetex': True,
+    'font.family': 'serif',
+    'font.serif': ['Palatino'],
+})
+
+
+def parse_args():
+    """Parse args"""
+    parser = argparse.ArgumentParser(
+        description='Plot amphibious simulation data',
+        formatter_class=(
+            lambda prog:
+            argparse.HelpFormatter(prog, max_help_position=50)
+        ),
+    )
+    parser.add_argument(
+        '--data',
+        type=str,
+        help='Data',
+    )
+    parser.add_argument(
+        '--animat',
+        type=str,
+        help='Animat options',
+    )
+    parser.add_argument(
+        '--simulation',
+        type=str,
+        help='Simulation options',
+    )
+    parser.add_argument(
+        '--output',
+        type=str,
+        help='Output path',
+    )
+    parser.add_argument(
+        '--drive_config',
+        type=str,
+        default='',
+        help='Descending drive method',
+    )
+    return parser.parse_args()
+
+
+def main():
+    """Main"""
+
+    # Clargs
+    clargs = parse_args()
+
+    # Load data
+    animat_options = AmphibiousOptions.load(clargs.animat)
+    animat_data = AmphibiousData.from_file(clargs.data)
+
+    # Plot connectivity
+    plots_network = (
+        plot_networks_maps(
+            data=animat_data,
+            animat_options=animat_options,
+            show_all=True,
+        )[1]
+        if animat_options.morphology.n_dof_legs <= 4
+        else {}
+    )
+
+    # Save plots
+    extension = 'pdf'
+    for name, fig in plots_network.items():
+        filename = os.path.join(clargs.output, f'{name}.{extension}')
+        pylog.debug('Saving to %s', filename)
+        fig.savefig(filename, format=extension, bbox_inches='tight', dpi=300)
+
+
+if __name__ == '__main__':
+    main()
