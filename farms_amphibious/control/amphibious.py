857bb1ac48af0ab7fa5ad3c77d866e034189dd2f
Chuanfang Ning <ningchuanfang@hotmail.com>
2026-01-20 15:02:54 +0100

    [CONTROL] Fix joint_names to be morphology.joints_names in joint map

--- farms_amphibious/control/amphibious.py
+++ farms_amphibious/control/amphibious.py
@@ -111,7 +111,7 @@ class JointMuscleController(AnimatController):
             animat_data: AmphibiousData,
             animat_network: AnimatNetwork,
     ):
-        joint_names = animat_options.sensor.joints_names()
+        joint_names = animat_options.morphology.joints_names()
         joints_control_names = animat_options.control.joints_names()
         joints_control_types: Dict[str, List[ControlType]] = {
             motor.joint_name: ControlType.from_string_list(motor.control_types)
